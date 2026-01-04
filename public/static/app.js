c10d97685745c98782c94b842d2aea5b8c123a59
Heejae Lee <133392872+STODYING@users.noreply.github.com>
2026-01-04 21:04:50 +0900

    Update app.js

--- public/static/app.js
+++ public/static/app.js
@@ -4,12 +4,12 @@
 
 // Firebase 설정 (환경 변수 사용)
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
-  authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
-  projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "process.env.REACT_APP_FIREBASE_APP_ID"
+  apiKey: "NETLIFY_SUB_API_KEY",
+  authDomain: "NETLIFY_SUB_AUTH_DOMAIN",
+  projectId: "NETLIFY_SUB_PROJECT_ID",
+  storageBucket: "NETLIFY_SUB_STORAGE_BUCKET",
+  messagingSenderId: "NETLIFY_SUB_MESSAGING_SENDER_ID",
+  appId: "NETLIFY_SUB_APP_ID"
 };
 
 // Firebase 초기화
